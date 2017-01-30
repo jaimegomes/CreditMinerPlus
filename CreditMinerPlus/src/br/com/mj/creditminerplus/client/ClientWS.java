f5f9ae2a8db3901c60f7db359e2fd0b9a6de80a0
Jaime Gomes <jaimegomes.bjj@gmail.com>
2017-01-30 17:47:26 -0200

    tratamento de erros!

--- CreditMinerPlus/src/br/com/mj/creditminerplus/client/ClientWS.java
+++ CreditMinerPlus/src/br/com/mj/creditminerplus/client/ClientWS.java
@@ -14,21 +14,30 @@ public class ClientWS {
 	public static List<Cliente> getInformacoesClienteWS(String cpf) {
 
 		try {
-			String urlws = "http://ws.consulta.plus/v2/govsc/cadastro/" + cpf + "?apiKey=XlYYdir53yerhDz5sgkgktyudgoLF4o7vde4";
+			String urlws = "http://ws.consulta.plus/v2/govsc/cadastro/" + cpf
+					+ "?apiKey=XlYYdir53yerhDz5sgkgktyudgoLF4o7vde4";
+			
 
 			Client c = Client.create();
+			
 			WebResource wr = c.resource(urlws);
 
 			String json = wr.get(String.class);
 
 			Gson gson = new Gson();
-
+			
 			return gson.fromJson(json, new TypeToken<List<Cliente>>() {
 			}.getType());
+			
 		} catch (Exception e) {
 			System.out.println("CPF: " + cpf + " " + e.getMessage());
-			return null;
+			
+			if (e.getMessage().indexOf("Gateway") > 0) {
+				System.out.println("TRATANDO ERRO, REPETINDO CPF " + cpf);
+				getInformacoesClienteWS(cpf);
+			} 
 		}
+		return null;
 
 	}
 
